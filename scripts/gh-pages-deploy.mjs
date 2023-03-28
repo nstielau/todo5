4337d451af3a343ace49faa39bf893f340885e66
Nick Stielau <nick.stielau@gmail.com>
2023-03-27 21:15:19 -0700

    Deploying staging branch too

--- scripts/gh-pages-deploy.mjs
+++ scripts/gh-pages-deploy.mjs
@@ -4,14 +4,24 @@ import fs from 'fs'
 
 (async () => {
   try {
-    await execa("git", ["checkout", "--orphan", "gh-pages"]);
     // eslint-disable-next-line no-console
-    console.log("Building started...");
+    console.log("Building main...");
     await execa("npm", ["run", "build"]);
     // Understand if it's dist or build folder
     const folderName = fs.existsSync("dist") ? "dist" : "build";
+
+    // eslint-disable-next-line no-console
+    console.log("Building staging...");
+    await execa("git", ["checkout", "staging"]);
+    await execa("npm", ["exec", "-c", "vite build --base /todo5/staging/ --outDir dist/staging"])
+
+    // eslint-disable-next-line no-console
+    console.log("Adding changes...");
+    await execa("git", ["checkout", "--orphan", "gh-pages"]);
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
+
+    // eslint-disable-next-line no-console
     console.log("Pushing to gh-pages...");
     await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
     await execa("rm", ["-r", folderName]);

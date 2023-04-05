4d36f0b9927f1e7a4b07ef39485b9824615a0b53
Nick Stielau <nick.stielau@gmail.com>
2023-04-04 22:04:41 -0700

    Whitespace.

--- src/components/partials/CurrentTaskComponent.vue
+++ src/components/partials/CurrentTaskComponent.vue
@@ -18,7 +18,7 @@ export default {
 <template>
   <div class="row q-mb-md" id="current_task">
     <div class="col-12">
-        {{prompt}}
+      {{prompt}}
     	<q-card>
     		<q-badge color="accent" class="float-right" v-if="isRecurring">Recurring <q-icon name="autorenew" /></q-badge>
     		<q-badge v-if="task.project">{{task.project}}</q-badge>

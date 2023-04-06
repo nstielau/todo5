5944a8013920afca0b278e2ee0a7ac1aa0d134ff
Nick Stielau <nick.stielau@gmail.com>
2023-04-06 13:00:09 -0700

    Adding overview before and after reviewing today's tasks.

--- src/components/TodayReviewComponent.vue
+++ src/components/TodayReviewComponent.vue
@@ -9,6 +9,9 @@ export default {
   data() {
     return {
       tasks: [],
+      show_overview: true,
+      review_completed: false,
+      today: true,
       projects: [],
       current_task_idx: 0,
       complete: false,
@@ -73,8 +76,10 @@ export default {
         }).catch((error) => console.log(error))
     },
     next_task() {
+      this.today = true
       if (this.tasks.length - 1 == this.current_task_idx) {
-        this.$emit('complete');
+        this.show_overview = true
+        this.review_completed = true
       }
       this.current_task_idx = this.current_task_idx + 1
     },
@@ -84,22 +89,73 @@ export default {
 
 <template>
 <div v-if="current_task">
+  <div v-if="show_overview">
+    <ReviewHeaderComponent :total="tasks.length" :current_idx="current_task_idx" title="Review Today's Tasks"/>
+    <ul>
+        <li class="q-ma-sm" v-for="task in tasks">
+          {{task.content}}
+        </li>
+    </ul>
+    <div class="col-12">
+      <div class="row q-py-md button-row">
+        <q-btn color="positive" label="Let's review" @click="this.show_overview = false"/>
+      </div>
+    </div>
+  </div>
+
+  <div v-else>
+    <div v-if="today">
+      <ReviewHeaderComponent :total="tasks.length" :current_idx="current_task_idx" title="Review Today's Tasks"/>
+      <q-slide-item @left="next_task()" @right="today=false" :key="current_task_idx">
+        <template v-slot:left>Yes<q-icon name="done" /></template>
+        <template v-slot:right>No<q-icon name="alarm" /></template>
+        <CurrentTaskComponent :task="current_task" prompt="Are we gonna get to it today?"/>
+      </q-slide-item>
+    </div>
+
+    <div v-else>
+      <ReviewHeaderComponent :total="tasks.length" :current_idx="current_task_idx" title="Review Today's Tasks"/>
+      <CurrentTaskComponent :task="current_task" prompt="Then when?"/>
+      <div class="col-12">
+        <div class="row q-py-md button-row">
+          <q-btn color="primary" label="Do today" @click="next_task()"/>
+          <q-btn color="primary" label="Do tomorrow" @click="set_due_date('tomorrow')"/>
+        </div>
+        <div class="row q-py-md button-row">
+          <q-btn color="primary" label="Do next week" @click="set_due_date('next week')"/>
+          <q-btn color="primary" label="In 7 days" @click="set_due_date('in 7 days')"/>
+        </div>
+        <div class="row q-py-md button-row">
+          <q-btn color="positive" label="Done" @click="close_task()"/>
+          <q-btn color="warning" label="Ack All" @click="$emit('complete')"/>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
+
+<div v-else-if="tasks.length > 0">
   <ReviewHeaderComponent :total="tasks.length" :current_idx="current_task_idx" title="Review Today's Tasks"/>
-  <CurrentTaskComponent :task="current_task" prompt="Are we gonna get to it today?"/>
+  <ul>
+      <li class="q-ma-sm" v-for="task in tasks">
+        {{task.content}}
+      </li>
+  </ul>
   <div class="col-12">
     <div class="row q-py-md button-row">
-      <q-btn color="primary" label="Do today" @click="this.next_task"/>
-      <q-btn color="primary" label="Do tomorrow" @click="set_due_date('tomorrow')"/>
-    </div>
-    <div class="row q-py-md button-row">
-      <q-btn color="primary" label="Do next week" @click="set_due_date('next week')"/>
-      <q-btn color="primary" label="In 7 days" @click="set_due_date('in 7 days')"/>
-    </div>
-    <div class="row q-py-md button-row">
-      <q-btn color="positive" label="Done" @click="close_task()"/>
-      <q-btn color="warning" label="Ack All" @click="$emit('complete')"/>
+      <q-btn color="positive" label="All done!" @click="this.$emit('complete');"/>
     </div>
   </div>
 </div>
-<SpinnerComponent title="No Today Tasks To Review!" :completed="complete" v-else/>
+
+<SpinnerComponent v-else title="No Today Tasks To Review!" :completed="complete" />
 </template>
+
+<style type="text/css">
+  li {
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+  }
+</style>
